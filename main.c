680f549431ef67db50429486a2a92cabae5ab33f
melchisedech333 <melchisedech333@gmail.com>
2022-10-10 02:02:33 -0300

    Updates

--- main.c
+++ main.c
@@ -10,23 +10,28 @@ int initialization (void)
     device_arithmetic_reset(device);
 
     // Configure device.
-    device_arithmetic_set_data(device, ARITHMETIC_INPUT1, "11111111");
-    device_arithmetic_set_data(device, ARITHMETIC_INPUT2, "11111111");
+    device_arithmetic_set_data(device, ARITHMETIC_INPUT1, "101");
+    device_arithmetic_set_data(device, ARITHMETIC_INPUT2, "011");
     device_arithmetic_set_operation(device, ARITHMETIC_OPERATION_SUM);
 
     // Process device.
     run_device_arithmetic(device);
 
-    printf("\nOperation: %s\n", get_operation_name(device->operation));
-    printf("\n\t %s\n\t %s\n", device->input1, device->input2);
-    printf("\t ----------\n\t %s\n\n", device->output);
-    printf("Decimal A: %d %c %d = %d\n",
-        bindec(device->input1), get_operation_op(device->operation), 
-        bindec(device->input2), bindec(device->output));
-    printf("Decimal B: %d %c %d = %d\n", 
-        bindec(device->input1), get_operation_op(device->operation), 
-        bindec(device->input2), 
-        bindec(device->input1) + bindec(device->input2));
+    // Show information.
+    printf("\nOperation: %s\n\n", get_operation_name(device->operation));
+    
+    printf("\t BINARY     | DEC\n");
+    printf("\t ----------------\n");
+    printf("\t            |\n");
+
+    printf("\t %s", device->input1);
+    printf(" | %d\n", bindec(device->input1));
+    printf("\t %s", device->input2);
+    printf(" | %d\n", bindec(device->input2));
+
+    printf("\t ----------------\n");
+    printf("\t %s", device->output);
+    printf(" | %d\n\n", bindec(device->output));
 
     return 0;
 }
