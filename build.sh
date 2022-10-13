3b51b7fda9b6a96d574b6c583d634c9f5308e258
melchisedech333 <melchisedech333@gmail.com>
2022-10-13 13:16:22 -0300

    Updates

--- build.sh
+++ build.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-gcc -c debug.c                  -o debug.o
-gcc -c utils.c                  -o utils.o
-gcc -c transistor-creation.c    -o transistor-creation.o
-gcc -c wire-creation.c          -o wire-creation.o
-gcc -c gate-construction.c      -o gate-construction.o
-gcc -c gate-machine.c           -o gate-machine.o
-gcc -c device-arithmetic.c      -o device-arithmetic.o
+rm -rf machine
+cd source
+
+HEADER="-I../header"
+
+gcc -c debug.c                  -o debug.o                  $HEADER
+gcc -c utils.c                  -o utils.o                  $HEADER
+gcc -c transistor-creation.c    -o transistor-creation.o    $HEADER
+gcc -c wire-creation.c          -o wire-creation.o          $HEADER
+gcc -c gate-construction.c      -o gate-construction.o      $HEADER
+gcc -c gate-machine.c           -o gate-machine.o           $HEADER
+gcc -c device-arithmetic.c      -o device-arithmetic.o      $HEADER
 
 OBJECTS="  debug.o utils.o gate-machine.o gate-construction.o "
 OBJECTS+=" transistor-creation.o wire-creation.o              "
 OBJECTS+=" device-arithmetic.o                                "
 
-gcc main.c $OBJECTS -o machine -lm
+gcc main.c $HEADER $OBJECTS -o machine -lm
 
 rm -rf *.o
+mv machine ..
 
 
